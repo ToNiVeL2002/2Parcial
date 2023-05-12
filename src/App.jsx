3e6092af28529c28821be5a01a4a21b4d14552aa
samu <samuelantoni2002@gmail.com>
2023-05-12 11:37:46 -0400

    hasta aqui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import { Card } from './components/Card'
 import './App.css'
+import cosas from './assets/cosas.json'
 
 function App() {
 
   return (
     <div className='app-container'>
+      {
+        cosas.map((cosas)=>{
+          return <Card name={cosas.name} key={cosas.id} descripcio={cosas.descripcion} price={cosas.price}/>
+      })
+      }
+
       <Card/>
 
       <Card/>
+
+      <Card/>
+
     </div>
   )
 }
